ea4e55a8473b32a8c1fafb6bc17e67f35cd041e3
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-09-16 19:16:35 +0000

    create trey page for portfolio

--- core/src/app/app.component.ts
+++ core/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+import { ViewportScroller } from '@angular/common'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent ,HomeComponent, AboutComponent, WorkComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'core';
+  
+  constructor(private viewportscroller: ViewportScroller){}
+
+  scrollerToSection(sectionId:string):void{
+      this.viewportscroller.scrollToAnchor(sectionId)
+  }
+   
 }
--- core/tailwind.config.js
+++ core/tailwind.config.js
@@ -1,10 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width:{
+        "screen":"600px",
+        "projet": "700px",
+      },
+      boxShadow:{
+        "custom": '-10px 10px 15px rgba(0,0,0,0.2), 10px -10px 20px rgba(0,0,0,0.4)',
+      },
+      transformOrigin:{
+        '3d': '50% 50%',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function({addUtilities}) {
+      const newUtilities = {
+        '.rotateY-45': {
+          transform: 'rotateY(55deg)',
+        }
+      }
+      addUtilities(newUtilities)
+    }
+    
+  ], 
 }
