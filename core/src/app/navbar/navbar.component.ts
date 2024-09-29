b42a1372de8b037eacfb2783fcd4c7512b2b17db
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-09-29 01:05:47 +0000

    nav bar create toggle

--- core/src/app/navbar/navbar.component.ts
+++ core/src/app/navbar/navbar.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  isMenuOpen = false;
     constructor(private viewportscroller: ViewportScroller){}
 
+
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen
+    }
     goTonextsection(sectionId: string){
       this.viewportscroller.scrollToAnchor(sectionId)
     }
