b0c16eef1556f48fe98bc19c4a6fd7b67030b873
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-10-03 21:46:26 +0000

    create skill container

--- core/src/app/app.component.ts
+++ core/src/app/app.component.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { WorkComponent } from './work/work.component';
 import { ContactComponent } from './contact/contact.component';
-import { ViewportScroller } from '@angular/common'
+import { ViewportScroller } from '@angular/common';
+import { SkillComponent } from './skill/skill.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent ,HomeComponent, AboutComponent, WorkComponent, ContactComponent],
+  imports: [RouterOutlet,NavbarComponent ,HomeComponent, AboutComponent, SkillComponent , WorkComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
