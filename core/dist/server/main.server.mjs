10404da90ea3e6b0e2c3a4218bc233a2fad6f673
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-10-06 17:37:30 +0000

    modification du section home

--- core/dist/server/main.server.mjs
+++ core/dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-E32FFDX2.mjs";import"./chunk-NCYE4O6O.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-XYAGB22Y.mjs";import"./chunk-NCYE4O6O.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
