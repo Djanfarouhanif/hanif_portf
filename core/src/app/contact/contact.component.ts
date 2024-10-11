341e2a1328fd7aa58648ca3e3f005a7a0a65edfd
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-10-11 17:20:02 +0000

    implementation des endpoint pour envoyer les requete

--- core/src/app/contact/contact.component.ts
+++ core/src/app/contact/contact.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+  myForm!: FormGroup 
+
+  constructor( private fb:FormBuilder, private http:HttpClient){
+
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      subject: ['', Validators.required],
+      message:  ['', Validators.required]
+    });
+
+  }
+
+  onSubmit(){
+    const formData = this.myForm.value;
+
+    this.http.post('http://localhost:8000/api/send-message', formData).subscribe(response =>{
+      console.log("Form send to backend", response);
+    });
+  }
 
 }
