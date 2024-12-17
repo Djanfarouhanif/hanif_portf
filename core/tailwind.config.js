0a38416e90a077eab3249452214a8828ce6a8075
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-12-17 21:44:19 +0000

    add the linkedin

--- core/tailwind.config.js
+++ core/tailwind.config.js
@@ -1,53 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Poppins' ],
-        pa: ['simple']
+        sans: ["Poppins"],
+        pa: ["simple"],
       },
-      width:{
-        "screen":"600px",
-        "projet": "700px",
+      width: {
+        screen: "600px",
+        projet: "700px",
       },
-      boxShadow:{
-        "custom": '-10px 10px 15px rgba(0,0,0,0.2), 10px -10px 20px rgba(0,0,0,0.4)',
+      boxShadow: {
+        custom: "-2px 2px 2px rgba(0,0,0,0.9), 2px -2px 5px rgba(0,0,0,0.9)",
       },
-      transformOrigin:{
-        '3d': '50% 50%',
+      transformOrigin: {
+        "3d": "50% 50%",
       },
       screens: {
-        'msc': {'max':'600px'},
-        'contact': {'max': '900px'}
+        msc: { max: "600px" },
+        contact: { max: "900px" },
       },
-      animation : {
-        wiggle: 'wiggle 10s ease-in-out infinite'
+      animation: {
+        wiggle: "wiggle 10s ease-in-out infinite",
       },
       keyframes: {
-        wiggle : {
-          '0%': {transform : 'translateX(0%)'},
-          '100%': { transform: 'translateX(-100%)'},
+        wiggle: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
           // '100%': {transform: 'translateX(0%)'}
-        }
-      }
+        },
+      },
     },
   },
   plugins: [
-    function({addUtilities}) {
+    function ({ addUtilities }) {
       const newUtilities = {
-        '.rotateY-45': {
-          transform: 'rotateY(55deg)',
+        ".rotateY-45": {
+          transform: "rotateY(55deg)",
         },
-        '.perspective-3d':{
-          perspective: '1000px'
-        }
-      }
-      addUtilities(newUtilities)
-    }
-    
-  ], 
-}
+        ".perspective-3d": {
+          perspective: "1000px",
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
+};
